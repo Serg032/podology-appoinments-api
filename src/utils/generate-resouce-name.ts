8576c69baf551628e3b5447e84407d31db5bebc4
Sergio Radigales <sergioradigales@gmail.com>
2025-06-02 23:08:00 +0200

    remove unused import

--- src/utils/generate-resouce-name.ts
+++ src/utils/generate-resouce-name.ts
@@ -1,5 +1,3 @@
-import { Construct } from "constructs";
-
 export const generateResourceName = (name: string, resourceType: string) => {
   const projectName = process.env.PROJECT_NAME || "podologist"; // Nombre del proyecto
   const stage = process.env.ENV || "dev"; // Entorno (dev, staging, prod)
